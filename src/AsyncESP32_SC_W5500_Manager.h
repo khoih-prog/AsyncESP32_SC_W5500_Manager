10dc664950ae4d0aeab78d20bda740c72fd4e2f1
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-12-14 18:51:44 -0500

    v1.0.0 for `ESP32_S3 + LwIP W5500`
    #### Releases v1.0.0
    
    1. Initial coding to port [ESPAsync_WiFiManager](https://github.com/khoih-prog/ESPAsync_WiFiManager) to ESP32_S3 boards using `LwIP W5500 Ethernet`.
    2. Use `allman astyle`

---
+++ src/AsyncESP32_SC_W5500_Manager.h
@@ -0,0 +1,31 @@
+/****************************************************************************************************************************
+  AsyncESP32_SC_W5500_Manager.h
+
+  For Ethernet shields using ESP32_SC_W5500 (ESP32_S2/3, ESP32_C3 + LwIP W5500)
+
+  Modified from
+  1. Tzapu               (https://github.com/tzapu/WiFiManager)
+  2. Ken Taylor          (https://github.com/kentaylor)
+  3. Alan Steremberg     (https://github.com/alanswx/ESPAsyncWiFiManager)
+  4. Khoi Hoang          (https://github.com/khoih-prog/ESPAsync_WiFiManager)
+
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncESP32_SC_W5500_Manager
+  Licensed under MIT license
+
+  Version: 1.0.0
+
+  Version Modified By  Date      Comments
+  ------- -----------  ---------- -----------
+  1.0.0   K Hoang     14/12/2022 Initial coding for ESP32_S3_W5500 (ESP32_S3 + W5500)
+ *****************************************************************************************************************************/
+
+#pragma once
+
+#ifndef AsyncESP32_SC_W5500_Manager_h
+#define AsyncESP32_SC_W5500_Manager_h
+
+#include <AsyncESP32_SC_W5500_Manager.hpp>              //https://github.com/khoih-prog/AsyncESP32_SC_W5500_Manager
+#include <AsyncESP32_SC_W5500_Manager_Impl.h>           //https://github.com/khoih-prog/AsyncESP32_SC_W5500_Manager
+
+#endif    // AsyncESP32_SC_W5500_Manager_h
+
